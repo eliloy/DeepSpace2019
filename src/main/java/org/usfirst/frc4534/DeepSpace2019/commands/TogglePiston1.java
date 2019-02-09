51595ed31af7eefef89c852bc98eb7239f0b32da
Ethan Pardieu <44349097+EthanPardieu@users.noreply.github.com>
2019-02-09 10:15:08 -0600

    I added a bunch of solenoid commands

---
+++ src/main/java/org/usfirst/frc4534/DeepSpace2019/commands/TogglePiston1.java
@@ -0,0 +1,51 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc4534.DeepSpace2019.commands;
+import org.usfirst.frc4534.DeepSpace2019.Robot;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class TogglePiston1 extends Command {
+  protected boolean state;
+  protected boolean isFinished; 
+  public TogglePiston1() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(Robot.pistonMove);
+  
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    state = Robot.pistonMove.getSolenoid1();
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    Robot.pistonMove.setSolenoid1(!state);
+    isFinished = true; 
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return isFinished;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
